56e6335000a5437410bc7871d332ccd0e2d8b280
PorthoGamesBR <84247243+PorthoGamesBR@users.noreply.github.com>
2022-11-12 17:37:46 -0300

    Create Class Whats

---
+++ whats.py
@@ -0,0 +1,21 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+
+class Whats:
+    
+    nav = webdriver.Chrome(ChromeDriverManager().install())
+    
+    def __init__(self):
+        pass
+    
+    def get_message_from():
+        pass
+    
+    def send_message_to():
+        pass
+    
+    def await_message():
+        pass
+    
+wp = Whats()
