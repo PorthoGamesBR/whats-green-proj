7dc12ba86803477401facca7620aff5306f0e419
PorthoGamesBR <84247243+PorthoGamesBR@users.noreply.github.com>
2022-11-13 15:37:12 -0300

    Get all visible contacts as WebElements

--- main.py
+++ main.py
@@ -3,7 +3,10 @@ from whats import Whats
 wts = Whats()
 
 def main():
-    pass
+    # Test code, do NOT send to main
+    contacts = wts.get_all_contacts_web()
+    for c in contacts:
+        print(c.text)
 
 if __name__ == "__main__":
     main()
--- whats.py
+++ whats.py
@@ -2,6 +2,8 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.remote.webelement import WebElement
+
 import time
     
 class WhatsElementsID:
@@ -10,6 +12,8 @@ class WhatsElementsID:
     messages = "main"
     
 class WhatsElementsClass:
+    
+    contact_text = "_3OvU8"
     
     contact_mesg_desc = "Hy9nV"
 
@@ -20,9 +24,20 @@ class Whats:
         self.nav.get('https://web.whatsapp.com')
         while len(self.nav.find_elements(By.ID,'side')) < 1:
             time.sleep(1)
-
             
-    
+        #We need to find a way to scroll the whole contact list
+            
+    def get_all_contacts_web(self) -> list[WebElement]:
+        # Get all visible contacts as WebElement objects (Name and last sent message)
+        contacts = ''
+        
+        # Will await until the contacts are visible
+        while len(contacts) < 1:
+            contacts = self.nav.find_elements(By.CLASS_NAME, WhatsElementsClass.contact_text)
+            time.sleep(1)
+        
+        return contacts
+        
     # Send message to a contact or group
     def send_message_to():
         pass
