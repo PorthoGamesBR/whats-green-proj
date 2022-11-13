7733cd9e25cb5fc7ca5b3abd06172fb7110cfb41
PorthoGamesBR <84247243+PorthoGamesBR@users.noreply.github.com>
2022-11-13 18:36:10 -0300

    Removing test code

--- main.py
+++ main.py
@@ -3,20 +3,9 @@ from selenium.webdriver.common.by import By
 from time import sleep
 
 wts = Whats()
-
+running = False
 def main():
-    # Test code, do NOT send to main
-    sleep(30)
-    contacts = wts.get_all_contacts_web()
-    if contacts:
-        for c in contacts:
-            try:
-                print('Contact:',c.find_element(By.CLASS_NAME, "zoWT4").text)
-                print('Last Sent Message',c.find_element(By.CLASS_NAME, "Hy9nV").text)
-                print()
-            except Exception:
-                print(c.text)
-                print()
+    running = True
 
 if __name__ == "__main__":
     main()
