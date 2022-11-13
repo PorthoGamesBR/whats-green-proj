49af8155b9f005713d0271780d090f766ada14ee
PorthoGamesBR <84247243+PorthoGamesBR@users.noreply.github.com>
2022-11-13 10:47:48 -0300

    Removing test code

--- whats.py
+++ whats.py
@@ -20,17 +20,7 @@ class Whats:
         self.nav.get('https://web.whatsapp.com')
         while len(self.nav.find_elements(By.ID,'side')) < 1:
             time.sleep(1)
-            
-        # Test code, get rid of it later        
-        mesg_descs = ""
-        while len(mesg_descs) < 1:
-            mesg_descs = self.nav.find_elements(By.CLASS_NAME, WhatsElementsClass.contact_mesg_desc)
-            time.sleep(1)
-        
-        print(len(mesg_descs))
-        for msg in mesg_descs:
-            print(msg.text)
-        #We need to find a way to scroll the whole contact list
+
             
     
     # Send message to a contact or group
